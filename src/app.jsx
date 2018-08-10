da07b58a229610ecc13b1acf1af218f477670870
yangwenyuan <yangwenyuan@mitong.com>
2018-08-10 18:00:37 +0800

    add react basic component

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,103 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
+
+class Component extends React.Component {
+  // 构造函数
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+    this.state = {
+      name: 'Issac'
+    }
+  }
+
+  // 组件将要加载
+  componentWillMount() {
+    console.log('componentWillMount');
+  }
+
+  // 组件加载完成
+  componentDidMount() {
+    console.log('componentDidMount');
+
+  }
+
+  // 将要接收父组件传来的props
+  componentWillReceiveProps() {
+    console.log('componentWillReceiveProps');
+  }
+
+  // 子组件是否应该更新
+  shouldComponentUpdate() {
+    console.log('shouldComponentUpdate');
+    return true;
+  }
+
+  //组件将要更新
+  componentWillUpdate() {
+    console.log('componentWillUpdate');
+  }
+
+  // 组件更新完成
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+  }
+
+  // 点击
+  handleClick() {
+    console.log('handleClick');
+    this.setState({
+      name: 'Yang'
+    });
+  }
+
+  render() {
+    console.log('render');
+    return (<div>
+      <div>{this.props.name}</div>
+      <button onClick={() => { this.handleClick() }}>更新组件</button>
+    </div>
+    );
+  }
+}
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+    this.state = {
+      name: 'Issac Parent',
+      hasChild: true
+    };
+  }
+
+  onPropsChange(){
+    console.log('onPropsChange');
+    this.setState({
+      name:'New Issac '
+    })
+  }
+
+  onDestroyChild() {
+    console.log('onDestroyChild');
+    this.setState({
+      hasChild:false
+    })
+  }
+
+  render() {
+    return <div>
+      {
+        this.state.hasChild ?  <Component name={this.state.name} />: null
+      }
+      <button onClick={()=>{this.onPropsChange();}}>改变Props</button>
+      <button onClick={()=>{this.onDestroyChild();}}>干掉子组件</button>
+    </div>
+  };
+}
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('app')
+  <App />,
+  document.getElementById("app")
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,82 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   entry: './src/app.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'app.js'
+    publicPath:'/dist/',
+    filename: 'js/app.js'
   },
   module: {
-    rules: [{
-      test: /\.jsx$/,
-      exclude: /(node_modules)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['env', 'react']
+    rules: [
+      // react处理
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
         }
+      },
+      // css处理
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      // sass处理
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+      // 图片处理
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            name: 'resource/[name].[ext]'
+          }
+        }]
+      },
+      // 字体图片处理
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|otf)$/i,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            name: 'resource/[name].[ext]'
+          }
+        }]
       }
-    }, {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader']
-    }]
+    ]
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: './src/index.html'
-  })]
+  plugins: [
+    // 处理html
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    // 独立css文件
+    new ExtractTextPlugin("css/[name].css"),
+    // 提出公共模块
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',
+      filename: 'js/base.js'
+    })
+  ],
+  devServer: {
+      port: 8099
+  }
 };
